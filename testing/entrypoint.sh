f3d435ddee92abd019ddafe13b306c65eff9b9d8
Mike Bertram <contact@mibexx.de>
2019-09-02 10:18:54 +0200

    * Entrypoint changes

--- testing/entrypoint.sh
+++ testing/entrypoint.sh
@@ -2,9 +2,12 @@
 set -e
 
 ansible-playbook -i $ANSIBLE_DIR/inventory $PLAYBOOK_DIR/testing.yml
+
+echo "Create database"
 sudo -u postgres createdb docker
 
 # Run your tests
+echo "Start testing log into: $LOG_DIR"
 [ -f $TESTING_BIN ] && bash $TESTING_BIN > $LOG_DIR/testing.log
 
 exec "$@"
